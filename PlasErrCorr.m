936b10a881700dbe29d30735a9cf802cb3507cb8
KSchmalbach <72946127+KSchmalbach@users.noreply.github.com>
2022-01-12 11:05:42 -0600

    Add files via upload
    Changed initial guess for solver to prevent finding complex root

--- PlasErrCorr.m
+++ PlasErrCorr.m
@@ -1,60 +1,59 @@
-function [hc,H,Ac]=PlasErrCorr(ARAdir,Disp,Load,Er)
+function [hc,H,Ac]=PlasErrCorr(ARAdir,Disp,Load,Er,LoadAmp)
     arguments
-        ARAdir char
-        Disp cell
-        Load cell
-        Er double 
+        ARAdir char %directory with area function
+        Disp cell %displacement
+        Load cell %load
+        Er double %reduced modulus
+        LoadAmp cell %load amplitude
     end
 
 
-Coeffs=ReadARA(ARAdir);
-nfiles=length(Disp);
-
-%Perfect Berk
-m0=24.5;
-beta=1;
-eps=0.726;
-
-Ac=cell(nfiles,1);
-hc=cell(nfiles,1);
-H=cell(nfiles,1);
-
-for FileNumber=1:nfiles
-    for Point=1:length(Load{FileNumber})
-        Qm4=pi()*eps^2*(Load{FileNumber}(Point)*1e-6)^2/(4*beta^2*(Er*1e9)^2);
-        Qnm4=Qm4*1e36;
-        quot=4*sqrt(Qnm4);
-        %analytic=0.5*(Disp(Point,FileNumber)+sqrt(Disp(Point,FileNumber)^2-quot));
-        hc{FileNumber}(Point)=Disp{FileNumber}(Point)-20;
-        f=AreaFunction(hc{FileNumber}(Point),Disp{FileNumber}(Point),Qnm4,Coeffs);
-        diffstep=1;
-        fpstep=AreaFunction(hc{FileNumber}(Point)+diffstep,Disp{FileNumber}(Point),Qnm4,Coeffs);
-        fmstep=AreaFunction(hc{FileNumber}(Point)-diffstep,Disp{FileNumber}(Point),Qnm4,Coeffs);
-        fprime=(fpstep-f)/(diffstep);
-        Step=f/fprime;
-        iter(Point)=1;
-        hc{FileNumber}(Point)=hc{FileNumber}(Point)-Step;
-
-        while abs(f)>1e1
-            hc{FileNumber}(Point)=hc{FileNumber}(Point)-Step;
+    Coeffs=ReadARA(ARAdir);
+    nfiles=length(Disp);
+    
+    %Perfect Berk
+    m0=24.5;
+    beta=1;
+    eps=0.726;
+    
+    Ac=cell(nfiles,1);
+    hc=cell(nfiles,1);
+    H=cell(nfiles,1);
+    
+    for FileNumber=1:nfiles
+        for Point=1:length(Load{FileNumber})
+            Qm4=pi()*eps^2*(Load{FileNumber}(Point)*1e-6)^2/(4*beta^2*(Er*1e9)^2);
+            Qnm4=Qm4*1e36;
+            quot=4*sqrt(Qnm4);
+            %analytic=0.5*(Disp(Point,FileNumber)+sqrt(Disp(Point,FileNumber)^2-quot));
+            hc{FileNumber}(Point)=Disp{FileNumber}(Point)-10;
             f=AreaFunction(hc{FileNumber}(Point),Disp{FileNumber}(Point),Qnm4,Coeffs);
+            diffstep=1;
             fpstep=AreaFunction(hc{FileNumber}(Point)+diffstep,Disp{FileNumber}(Point),Qnm4,Coeffs);
             fmstep=AreaFunction(hc{FileNumber}(Point)-diffstep,Disp{FileNumber}(Point),Qnm4,Coeffs);
-            fprime=(fpstep-fmstep)/(2*diffstep);
+            fprime=(fpstep-f)/(diffstep);
             Step=f/fprime;
-            iter(Point)=iter(Point)+1;
-            if iter(Point)>100
-                break
+            iter(Point)=1;
+            hc{FileNumber}(Point)=hc{FileNumber}(Point)-Step;
+    
+            while abs(f)>1e1
+                hc{FileNumber}(Point)=hc{FileNumber}(Point)-Step;
+                f=AreaFunction(hc{FileNumber}(Point),Disp{FileNumber}(Point),Qnm4,Coeffs);
+                fpstep=AreaFunction(hc{FileNumber}(Point)+diffstep,Disp{FileNumber}(Point),Qnm4,Coeffs);
+                fmstep=AreaFunction(hc{FileNumber}(Point)-diffstep,Disp{FileNumber}(Point),Qnm4,Coeffs);
+                fprime=(fpstep-fmstep)/(2*diffstep);
+                Step=f/fprime;
+                iter(Point)=iter(Point)+1;
+                if iter(Point)>100
+                    break
+                end
             end
+            Ac{FileNumber}(Point)=Area(Coeffs,hc{FileNumber}(Point)); %nm^2
+            H{FileNumber}(Point)=((Load{FileNumber}(Point)+LoadAmp{FileNumber}(Point))*1e-6)...
+                /(Ac{FileNumber}(Point)*1e-9);
+            
         end
-        Ac{FileNumber}(Point)=Area(Coeffs,hc{FileNumber}(Point)); %nm^2
-        H{FileNumber}(Point)=(Load{FileNumber}(Point)*1e-6)/(Ac{FileNumber}(Point)*1e-9);
-        
     end
-end
-
-
-
 end
 
 function f=AreaFunction(hc,disp,Q,Coeffs)
